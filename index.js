e12a816477d3295b62cb82030aecc72f69bec33c
Neutral75 <neutral750@gmail.com>
2023-04-12 13:53:34 +0100

    📟 Added the dashboard page

--- index.js
+++ index.js
@@ -17,6 +17,68 @@ app.get('/', async (request, response) => {
     return response.render('index');
 });
 
+app.get('/login', async (request, response) => {
+    return response.redirect(`https://discord.com/api/oauth2/authorize?client_id=${config.clientID}&redirect_uri=${config.redirectURI}&response_type=code&scope=${config.scope}`);
+});
+
+app.get('/auth/discord', async (request, response) => {
+    const code = request.query.code;
+
+    const result = await fetch('https://discord.com/api/oauth2/token', {
+        method: 'POST',
+        body: new URLSearchParams({
+            client_id: config.clientID,
+            client_secret: config.clientSecret,
+            code: code,
+            grant_type: 'authorization_code',
+            redirect_uri: config.redirectURI,
+            scope: config.scope
+        }),
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        }
+    });
+
+    const data = await result.json();
+
+    response.cookie('token_type', data.token_type);
+    response.cookie('access_token', data.access_token, {
+        maxAge: data.expires_in * 1000,
+        httpOnly: true,
+    });
+
+    return response.redirect('/dashboard');
+});
+
+app.get('/dashboard', async (request, response) => {
+    const tokenType = request.cookies.token_type;
+    const accessToken = request.cookies.access_token;
+
+    if (!tokenType || !accessToken) {
+        return response.redirect('/login');
+    }
+
+    const userData = await fetch('https://discord.com/api/users/@me', {
+        headers: {
+            authorization: `${tokenType} ${accessToken}`
+        }
+    });
+
+    const guildsData = await fetch('https://discord.com/api/users/@me/guilds', {
+        headers: {
+            authorization: `${tokenType} ${accessToken}`
+        }
+    });
+
+    const user = await userData.json();
+    const guilds = await guildsData.json();
+
+    return response.render('dashboard', {
+        user: user,
+        guilds: guilds
+    });
+});
+
 app.listen(config.PORT, () => {
     return console.log('Beep! Running on http://localhost:' + config.PORT);
 });
